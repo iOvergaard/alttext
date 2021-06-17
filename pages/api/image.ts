b1877e7b1547b83cd06c792d093805b2e759db23
Jacob Overgaard <JOV@impact.dk>
2021-06-17 23:02:52 +0200

    Remove support for POST for now

--- pages/api/image.ts
+++ pages/api/image.ts
@@ -1,44 +1,51 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-import { imageToText, imageToTextBinary } from '../../lib/image-to-text';
+import { imageToText } from '../../lib/image-to-text';
 import { ImageCaption } from '../../lib/models/image-caption';
 import { runMiddleware } from '../../lib/run-middleware';
 
 const Cors = require('cors');
 
 const cors = Cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'HEAD']
+  origin: '*',
+  methods: ['GET', 'POST', 'HEAD']
 });
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<ImageCaption | string>) {
-    await runMiddleware(req, res, cors);
-
-    let caption: ImageCaption | null = null;
-    let errorMsg = '';
-
-    if (req.method === 'GET') {
-        const { describeURL = 'https://moderatorsampleimages.blob.core.windows.net/samples/sample16.png' } = req.query;
-
-        // Analyze URL image
-        console.log('Analyzing URL image to describe...', describeURL);
-
-        try {
-            caption = await imageToText(Array.isArray(describeURL) ? describeURL[0] : describeURL);
-        } catch (e) {
-            errorMsg = e.message;
-        }
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb'
     }
-
-    else if (req.method === 'POST') {
-        const { file } = req.body;
-
-        try {
-            caption = await imageToTextBinary(file);
-        } catch (e) {
-            errorMsg = e.message;
-        }
-    }
-
-    return caption ? res.status(200).json(caption) : res.status(407).send('Something went wrong trying to contact the API (' + errorMsg + ')')
+  }
 };
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ImageCaption | string>) {
+  await runMiddleware(req, res, cors);
+
+  let caption: ImageCaption | null = null;
+  let errorMsg = '';
+
+  if (req.method === 'GET') {
+    const { describeURL = 'https://moderatorsampleimages.blob.core.windows.net/samples/sample16.png' } = req.query;
+
+    // Analyze URL image
+    console.log('Analyzing URL image to describe...', describeURL);
+
+    try {
+      caption = await imageToText(Array.isArray(describeURL) ? describeURL[0] : describeURL);
+    } catch (e) {
+      errorMsg = e.message;
+    }
+  } else {
+    res.status(405);
+    res.end();
+    return;
+  }
+
+  if (caption) res.status(200).json(caption);
+  else {
+    res.statusMessage = 'Something went wrong trying to contact the API (' + errorMsg + ')';
+    res.status(500);
+    res.end();
+  }
+}
