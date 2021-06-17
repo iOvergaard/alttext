4ce682cd61e96e57cfba52947dff4254c3267c00
Jacob Overgaard <JOV@impact.dk>
2021-06-17 23:02:35 +0200

    Fix errors everywhere

--- lib/image-to-text.ts
+++ lib/image-to-text.ts
@@ -8,41 +8,32 @@ const key = process.env.API_KEY;
 const endpoint = process.env.API_ENDPOINT;
 
 if (!key || !endpoint) {
-    throw new Error('You must specify a key & endpoint');
+  throw new Error('You must specify a key & endpoint');
 }
 
-const computerVisionClient = new ComputerVisionClient(
-    new ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': key } }), endpoint);
+const computerVisionClient = new ComputerVisionClient(new ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': key } }), endpoint);
 
 export async function imageToText(describeURL: string): Promise<ImageCaption> {
-    try {
-        const describeImage = (await computerVisionClient.describeImage(describeURL));
-        return extractCaption(describeImage);
-    } catch(e) {
-        throw new Error('Something went wrong with the API (' + e.message + ')');
-    }
+  const describeImage = await computerVisionClient.describeImage(describeURL);
+  return extractCaption(describeImage);
 }
 
 export async function imageToTextBinary(image: HttpRequestBody): Promise<ImageCaption> {
-    try {
-        const describeImage = await computerVisionClient.describeImageInStream(image);
-        return extractCaption(describeImage);
-    } catch(e) {
-        throw new Error('Something went wrong with the API (' + e.message + ')');
-    }
+  const describeImage = await computerVisionClient.describeImageInStream(image);
+  return extractCaption(describeImage);
 }
 
 function extractCaption(describeImage: DescribeImageResponse) {
-    if (!describeImage) {
-        throw new Error('Could not contact Azure API');
-    }
+  if (!describeImage) {
+    throw new Error('Could not contact Azure API');
+  }
 
-    const caption = describeImage.captions?.[0];
+  const caption = describeImage.captions?.[0];
 
-    if (!caption) {
-        throw new Error('Could not find a caption');
-    }
+  if (!caption) {
+    throw new Error('Could not find a caption');
+  }
 
-    console.log(`This may be ${caption.text} (${caption.confidence?.toFixed(2)} confidence)`);
-    return { caption: caption.text, confidence: caption.confidence?.toFixed(2) ?? '0.0' };
+  console.log(`This may be ${caption.text} (${caption.confidence?.toFixed(2)} confidence)`);
+  return { caption: caption.text, confidence: caption.confidence?.toFixed(2) ?? '0.0' };
 }
