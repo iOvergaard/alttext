f4654c0bda32622b75e980a5b4f72b751568b46e
Jacob Overgaard <jov@impact.dk>
2021-06-21 12:56:25 +0200

    Add Head to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
+import Head from 'next/head';
+
 import Urlzone from '../components/urlzone';
 import styles from './index.module.scss';
 
 export default function Home(): JSX.Element {
   return (
     <main className={styles.main}>
+      <Head>
+        <title>Alt Text Generator</title>
+      </Head>
       <header>
         <h1>Welcome</h1>
         <p>This tool enables you to find an alt text from images</p>
