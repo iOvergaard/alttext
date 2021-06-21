e0a36ade41b97be0fa55f7ffd62a94c9e5b14405
Jacob Overgaard <jov@impact.dk>
2021-06-21 10:43:06 +0200

    Add styling to main

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import Urlzone from '../components/urlzone';
+import styles from './index.module.scss';
 
 export default function Home(): JSX.Element {
   return (
-    <main>
+    <main className={styles.main}>
       <header>
         <h1>Welcome</h1>
         <p>This tool enables you to find an alt text from images</p>
